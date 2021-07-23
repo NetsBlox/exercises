f5113b2d201baace1ae1b353ebded697605340bc
Brian Broll <brian.broll@gmail.com>
2021-07-23 16:30:03 -0500

    Add empty template script. Rename templates to parsons

--- utils/build-library/index.js
+++ utils/build-library/index.js
@@ -1,20 +1,103 @@
 const path = require('path');
+const assert = require('assert');
 const fs = require('fs');
 const EXERCISES_PATH = path.join(__dirname, '..', '..', 'exercises');
 const TPL_PATH = path.join(__dirname, 'template.xml.ejs');
 const tpl = require('lodash.template');
 const makeLibrary = tpl(fs.readFileSync(TPL_PATH, 'utf8'));
+const XML_Element = require('./lib/snap/xml');
 
 const isHook = !!process.argv.find(opt => opt === '--hook');
 
+function sum(a, b) {
+    return a + b;
+}
 const updatedCount = [
-    updateLibrary(),
     cleanExercises(),
-].reduce((sum, next) => sum + next, 0);
+    //createEmptyTemplates(),
+    updateLibrary(),
+].reduce(sum, 0);
 
 if (isHook && updatedCount > 0) {
     console.log('Please confirm the changes and recommit.');
     process.exit(1);
+}
+
+/*
+ * Ensure each exercise has a template.xml file in which the custom
+ * block has all contents removed
+ */
+function createEmptyTemplates() {
+    const parsonsFiles = getParsonsFiles();
+    return parsonsFiles.map(createEmptyTemplate).reduce(sum, 0);
+}
+
+function getParsonsFiles() {
+    const getParsonsFile = dirname => {
+        const filenames = fs.readdirSync(path.join(EXERCISES_PATH, dirname));
+        const parsonsVariants = filenames
+            .filter(name => name.startsWith('parsons') && name.endsWith('.xml'))
+            .sort((a, b) => a.length < b.length ? -1 : 1)
+
+        if (parsonsVariants.length) {
+            const filename = parsonsVariants.shift();
+            return path.join(EXERCISES_PATH, dirname, filename);
+        }
+    };
+
+    const parsonsFiles = fs.readdirSync(EXERCISES_PATH)
+        .map(name => getParsonsFile(name))
+        .filter(filename => filename);
+
+    return parsonsFiles;
+}
+
+function getTestedBlocks(dirname) {
+    const testsFile = path.join(dirname, 'tests.json');
+    if (fs.existsSync(testsFile)) {
+        const blockSpecs = require(testsFile)
+            .map(test => test.spec);
+
+        return unique(blockSpecs);
+    } else {
+        return [];
+    }
+}
+
+function unique(list) {
+    return list.reduce((uniq, item) => {
+        if (!uniq.includes(item)) {
+            uniq.push(item);
+        }
+        return uniq;
+    }, []);
+}
+
+function createEmptyTemplate(parsonsFile) {
+    const dirname = path.dirname(parsonsFile);
+    const blockSpecs = getTestedBlocks(dirname);
+    const parsonsXml = fs.readFileSync(parsonsFile, 'utf8');
+
+    const element = new XML_Element();
+    element.parseString(parsonsXml);
+    assert(element.children.length === 1, 'Multiple roles not yet supported');
+
+    const [role] = element.children;
+    const [project] = role.children;
+    const blocks = project.childNamed('blocks');
+    const testedBlocks = blocks.children.filter(block => blockSpecs.includes(block.attributes.s));
+
+    testedBlocks.forEach(blockDef => {
+        const scripts = blockDef.childNamed('scripts');
+        scripts.children.forEach(block => scripts.removeChild(block));
+    });
+
+    const templateFile = path.join(dirname, 'template.xml');
+    return updateFile(
+        templateFile,
+        element.toString().trim(),
+        `Created blank template for ${path.basename(dirname)}`
+    );
 }
 
 function cleanExercises() {
@@ -61,6 +144,8 @@ function updateFile(path, newContents, msg) {
     const contents = () => fs.readFileSync(path, 'utf8').trim();
     let updated = false;
     if (!fs.existsSync(path) || newContents !== contents()) {
+        console.log(`>${newContents}<`);
+        console.log(`>${contents()}<`);
         fs.writeFileSync(path, newContents);
         console.log(msg);
         updated = true;
