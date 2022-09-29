29638cdafef71478b08358d6da5c3ade8e651752
Brian Broll <brian.broll@gmail.com>
2022-09-29 09:13:47 -0500

    initial commit of website

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+	console.log('page loaded');
+	// TODO: create a search field
+	// TODO: fetch the list of exercises?
+	// TODO: show them in a list
+	// TODO: add badges for each item in the list
+	// TODO: add semi-clever search/filtering capabilities
+	// TODO: button to open in NetsBlox (w/ autograding?)
+</script>
