eac06d435c3375d359d52c4a96997986d35daac1
Brian Broll <brian.broll@gmail.com>
2023-02-28 14:35:52 -0600

    rename parsons-func to parsons

--- docs/internal/immutable/chunks/2-73f61b39.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-4e95c1d1.js";export{t as component};
---
+++ docs/internal/immutable/chunks/2-afd7327e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-f0f33f63.js";export{t as component};
