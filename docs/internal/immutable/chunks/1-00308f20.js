ffc1b917abba163857c790d909864d63e54aa7ce
Brian Broll <brian.broll@gmail.com>
2023-02-28 14:01:20 -0600

    Add Factorial exercise

--- docs/internal/immutable/chunks/1-00308f20.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-c8ddd2bb.js";export{t as component};
---
+++ docs/internal/immutable/chunks/2-73f61b39.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-4e95c1d1.js";export{t as component};
