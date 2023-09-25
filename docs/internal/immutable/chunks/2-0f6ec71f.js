d3d2477aed5fcce314108846451045464c708efb
Brian Broll <brian.broll@gmail.com>
2023-09-25 12:40:41 -0500

    Add better descriptions for exercises (#46)
    * Add better descriptions for exercises
    
    * Fix code formatting
    
    * Build GitHub Pages
    
    ---------
    
    Co-authored-by: Format Bot <format-bot@netsblox.org>
    Co-authored-by: Build Bot <build-bot@netsblox.org>

--- docs/internal/immutable/chunks/2-0f6ec71f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-e511e767.js";export{t as component};
---
+++ docs/internal/immutable/chunks/3-873199b1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/create/_page.svelte-ebfe970d.js";export{t as component};
